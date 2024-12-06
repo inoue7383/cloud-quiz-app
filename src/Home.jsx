f8ceff3e17545f581595890fd08d21073aed98cf
inoue7383 <kyosukeinoue20050524@gmail.com>
2024-12-06 19:11:41 +0900

    Update Home.jsx to include icon and new text

--- src/Home.jsx
+++ src/Home.jsx
@@ -62,7 +62,7 @@ const Home = () => {
       <header className="header">
         <div className="header-content">
         <Link to="/" className="site-name">
-          <img src="/src/assets/icon-correct-24-1.png" alt="icon" className="site-icon" />
+          <img src="/assets/icon-correct-24-1.png" alt="icon" className="site-icon" />
           <h2>CloudQuest</h2>
         </Link>
           {user ? (
